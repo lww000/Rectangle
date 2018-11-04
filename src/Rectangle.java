0d1135420d4e338c1e411e743229e2c8daffc58c
lww000 <1481131808@qq.com>
2018-11-04 20:43:23 +0800

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
+
+    public Rectangle(int w, int h) {
+        this.width = w;
+        this.height = h;
+    }
+
+    @Override
+    public double getArea() {
+        // TODO
+        double area=width*height;
+        return area; // to be replaced
+    }
+
+}
+
---
+++ src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+
+    public static void main(String[] args) {
+        Rectangle area1=new Rectangle(10, 5);
+       System.out.println(area1.getArea());
+
+
+       // Shape s1 = new Rectangle(10, 5);
+        // Uncomment the code below when finished
+        // Shape s2 = new Circle(2);
+
+       // System.out.println("The size is "+s1.getArea());
+        // System.out.println("The size is "+s2.getArea());
+    }
+}
